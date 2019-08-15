15498c06258dc79b3b236c7bc855cd3fafd4f987
surendhar10 <53898697+surendhar10@users.noreply.github.com>
2019-08-15 15:52:07 +0530

    Update task2.c

--- task2.c
+++ task2.c
@@ -5,6 +5,8 @@ int main()
     int a;
     printf("enter a number: ");
     scanf("%d",&a);
+    if(a>0)
+    {
     if(a%2==0)
         {
             printf("the given number is even ");
@@ -13,6 +15,13 @@ int main()
         {
             printf("The given number is odd");
         }
+    }
+    else
+    {
+        printf("the given input is invalid");
+    }
     return 0;
     
 }
+
+
