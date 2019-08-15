8cb6e15e07b70cf9ac1eb6a18575618802bfa1f7
surendhar10 <53898697+surendhar10@users.noreply.github.com>
2019-08-15 10:54:48 +0530

    Create task1.c

---
+++ task1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void main() 
+{
+    float n;
+    printf("enter a number: ");
+    scanf("%f",&n);
+    if(n<=0)
+    {
+        if(n=0)
+        {
+            printf("the given number is zero ");
+        }
+        else
+        {
+            printf("The given number is negative");
+        }
+    }    
+    else
+        {
+            printf("the given number is positive");
+        }    
+    
+}
