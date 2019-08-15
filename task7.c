f01e8450bcf3e8f1b7e00d50b423e2bf420ec8b9
surendhar10 <53898697+surendhar10@users.noreply.github.com>
2019-08-15 17:02:36 +0530

    Create task7.c

---
+++ task7.c
@@ -0,0 +1,11 @@
+ #include <stdio.h>
+    void main()
+    {
+        int i,n;
+        printf("enter n value:");
+        scanf("%d",&n);
+        for(i=1;i<=n;i++)
+        {
+         printf("\n  hello");
+        }
+}
