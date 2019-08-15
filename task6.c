20993e941ea39d3e1c43973c287ba4ac09da8a12
surendhar10 <53898697+surendhar10@users.noreply.github.com>
2019-08-15 16:46:35 +0530

    Create task6.c

---
+++ task6.c
@@ -0,0 +1,15 @@
+ #include <stdio.h>
+    void main()
+    {
+        int a;
+         printf("Enter the year: ");
+         scanf("%d",&a);
+         if(a%4==0)
+         {
+             printf("It is a leap year");
+         }
+         else
+         {
+             printf("It is not a leap year");
+         }
+}
